6c680c9e61415f40b38619e8fb3ad46cada201b8
daltonv <daltonverhagen@gmail.com>
2017-03-27 14:00:29 -0400

    added datapath shell

---
+++ Components/datapath.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity datapath is
+	generic (
+		WIDTH : positive := 32
+	);
+	port (
+		input1 : in std_logic_vector(WIDTH-1 downto 0);
+		input2 : in std_logic_vector(WIDTH-1 downto 0);
+		input1_en : in std_logic;
+		input2_en : in std_logic;
+		output : out std_logic_vector(WIDTH-1 downto 0);
+	);
+end datapath;
+
+architecture STR of datapath is
+
+begin
+
+end STR;
