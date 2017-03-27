0300dfc5b65a2c57bac1b71e3fa02a22b9331001
daltonv <daltonverhagen@gmail.com>
2017-03-27 13:47:27 -0400

    added some functions to the alu

--- Components/alu.vhd
+++ Components/alu.vhd
@@ -56,4 +56,116 @@ architecture BHV of alu is
 
 begin
 
+	process(input1,input2,op)
+		variable arith_result : unsigned(WIDTH*2-1 downto 0);
+
+	begin
+
+	--init outputs to zero to prevent latches
+	branch_taken <= '0';
+	result_LO <= (others >= '0');
+	result_HI <= (others >= '0');
+
+	case op is
+		when C_ADD 		=> --$s1 = $s2 + $s3
+			arith_result := resize(unsigned(input1),arith_result'length)+resize(unsigned(input2),add_result'length);
+			result_LO <= std_logic_vector(arith_result(WIDTH-1 downto 0));
+			result_HI <= std_logic_vector(arith_result(WIDTH*2-1 downto WIDTH);
+
+		when C_ADD_IM 	=> --$s1 = $s2 + IMM
+			arith_result := resize(unsigned(input1),arith_result'length)+resize(unsigned(input2),add_result'length);
+			result_LO <= std_logic_vector(arith_result(WIDTH-1 downto 0));
+			result_HI <= std_logic_vector(arith_result(WIDTH*2-1 downto WIDTH);
+
+		when C_SUB 		=> --$s1 = $s2 - $s3
+			arith_result := resize(unsigned(input1),arith_result'length)-resize(unsigned(input2),add_result'length);
+			result_LO <= std_logic_vector(arith_result(WIDTH-1 downto 0));
+			result_HI <= std_logic_vector(arith_result(WIDTH*2-1 downto WIDTH);
+
+		when C_SUB_IM	=> --$s1 = $s2 - IMM
+			arith_result := resize(unsigned(input1),arith_result'length)-resize(unsigned(input2),add_result'length);
+			result_LO <= std_logic_vector(arith_result(WIDTH-1 downto 0));
+			result_HI <= std_logic_vector(arith_result(WIDTH*2-1 downto WIDTH);
+		
+		when C_MULT 	=> --$LO= $s * $t
+			arith_result := resize(unsigned(input1),arith_result'length)*resize(unsigned(input2),add_result'length);
+			result_LO <= std_logic_vector(arith_result(WIDTH-1 downto 0));
+			result_HI <= std_logic_vector(arith_result(WIDTH*2-1 downto WIDTH);
+		
+		when C_MULT_UN 	=> --$LO= $s * $t
+			arith_result := resize(unsigned(input1),arith_result'length)*resize(unsigned(input2),add_result'length);
+			result_LO <= std_logic_vector(arith_result(WIDTH-1 downto 0));
+			result_HI <= std_logic_vector(arith_result(WIDTH*2-1 downto WIDTH);
+		
+		when C_AND  	=> --$s1 = $s2 and $s3
+			result_LO <= input1 and input2;
+		
+		when C_ANDI  	=> --$s1 = $s2 and IMM
+			result_LO <= input1 and input2;
+		
+		when C_OR  		=> --$s1 = $s2 or $s3
+			result_LO <= input1 or input2;
+		
+		when C_ORI 		=> --$s1 = $s2 or IMM
+			result_LO <= input1 or input2;
+		
+		when C_XOR  	=> --$s1 = $s2 xor $s3
+			result_LO <= input1 xor input2;
+		
+		when C_XORI  	=> --$s1 = $s2 xor IMM
+			result_LO <= input1 xor input2;
+		
+		when C_SRL  	=> --$s1 = $s2 >> H (H is bits 10-6 of IR)
+			result_LO <= std_logic_vector(shift_right(unsigned(input1),unsigned(input2)));
+
+		when C_SLL  	=> --$s1 = $s2 << H (H is bits 10-6 of IR)
+			result_LO <= std_logic_vector(shift_left(unsigned(input1),unsigned(input2)));
+
+		when C_SRA  	=>
+		
+		when C_SLT  	=> --$s1=1 if $s2 < $s3 else $s1=0
+			if(unsigned(input1) < unsigned(input2)) then
+				result_LO <= std_logic_vector(1);
+			end if;
+		
+		when C_SLTI		=> --$s1=1 if $s2 < IMM else $s1=0
+			if(unsigned(input1) < unsigned(input2)) then
+				result_LO <= std_logic_vector(1);
+			end if;
+		
+		when C_SLTIU	=> --$s1=1 if $s2 < IMM else $s1=0
+		
+		when C_SLTU		=> --$s1=1 if $s2 < $s3 else $s1=0
+		
+		when C_MFHI		=> --$s1= HI
+		
+		when C_MFLO		=> --$s1= LO
+		
+		when C_LW		=> --$s1 = RAM[$s2+offset]
+		
+		when C_SW		=> -- RAM[$s2+offset] = $s1
+		
+		when C_BEQ		=> --if $s1=$s2, PC += TARGET
+		
+		when C_BNE		=> --if $s1/=$s2, PC += TARGET
+		
+		when C_BLEZ		=> --if $s1 <= 0, PC += TARGET
+		
+		when C_BGTZ		=> --if $s1 > 0, PC += TARGET
+		
+		when C_BLTZ		=> --if $s1 < 0, PC += TARGET
+		
+		when C_BGEZ		=> --if $s1 >= 0, PC += TARGET
+		
+		when C_J		=> --PC = TARGET
+		
+		when C_JAL		=> --$ra = PC+8 and PC = TARGET 
+		
+		when C_JR		=> --PC = $ra
+		
+		when C_HALT		=>
+
+	end case;
+
+	end process;
 end BHV;
